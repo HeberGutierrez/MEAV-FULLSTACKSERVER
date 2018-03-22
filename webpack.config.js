a0dac260bb10defa8a4a4f7ecbea942f06a5fbe8
hebergutierrez.p@gmail.com <hebergutierrez.p@gmail.com>
2018-03-21 22:46:49 -0600

    main webpack

--- webpack.config.js
+++ webpack.config.js
@@ -107,34 +107,3 @@ if ( process.env.NODE_ENV === 'production' ) {
 			} )
   ] )
 }
-
-var path = require( 'paht' )
-var webpack = require( 'webpack' )
-var merge - require( 'webpack-merge' )
-var baseWebpackConfig = require( './webpack.config' )
-var webpackConfig = merge( baseWebpackConfig, {
-	target: 'node',
-	entry: {
-		app: './src/entry-server.js'
-	},
-	devtool: false,
-	output: {
-		path: path.resolve( __dirname, './dist' ),
-		filename: 'server.bundle.js',
-		libraryTarget: 'commonjs2'
-	},
-	externals: Object.keys( require( './package.json' )
-		.dependencies ),
-	plugins: [
-    new webpack.DefinePlugin( {
-			'process.env': 'production'
-		} ),
-    new webpack.optimize.UglifyJsPlugin( {
-			compress: {
-				warnings: false
-			}
-		} )
-  ]
-} )
-
-module.exports = webpackConfig
